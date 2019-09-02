26e60fd5779f5f4f68ce6453111f915460b916dc
DESKTOP-KCS1KCG\Drako <mazzantia@hotmail.com>
2019-09-02 19:34:20 +0200

    xlarge design activity app

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bitmapImage= BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val roundedBitImage= RoundedBitmapDrawableFactory.create(resources,bitmapImage)
+        roundedBitImage.cornerRadius=15f
+        logo.setImageDrawable(roundedBitImage)
+
+
     }
 }
